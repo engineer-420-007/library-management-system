54cee06fd7036de28a41d136aa70efc323a128e5
MeetParmar2302 <meet.parmar.2302@gmail.com>
2025-03-16 21:52:33 +0530

    Updates Authform loading skeleton

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 import "/app/globals.css"; // Ensure correct path
 import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-import Skeleton from "@/components/ui/authform-skeleton";
+import Skeleton from "@/components/authform-skeleton";
 
 export default function Page() {
   return (
     <div className="">
       {/* Show Skeleton when Clerk is loading */}
       <ClerkLoading>
-        <Skeleton />
+        <Skeleton type = "signin"/>
       </ClerkLoading>
 
       {/* Show SignIn UI after Clerk has loaded */}
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,13 +1,13 @@
 import "/app/globals.css";
 import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
-import Skeleton from "@/components/ui/authform-skeleton";
+import Skeleton from "@/components/authform-skeleton";
 
 export default function Page() {
   return (
     <div className="">
       {/* Show Skeleton when Clerk is loading */}
       <ClerkLoading>
-        <Skeleton />
+        <Skeleton type = "signup"/>
       </ClerkLoading>
 
       {/* Show SignIn UI after Clerk has loaded */}
